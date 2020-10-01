81003c4af9f3aa24633a605726f77995b5036326
reste51 <61609268+reste51@users.noreply.github.com>
2020-10-01 21:57:19 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// element-ui的使用
+import elementUI from 'element-ui'
+// 导入样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(elementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // vue-cli 配置端口号 及 默认打开浏览器
+  devServer: {
+    port: 8888,
+    open: true
+  }
+}
