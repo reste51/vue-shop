db87cf24e524909c63ef8e2134096f0a1ef4222c
reste51 <61609268+reste51@users.noreply.github.com>
2020-10-02 10:47:53 +0800

    免密提交

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <el-row>
       <el-button>默认按钮 测试提交</el-button>
-      <el-button type="primary">主要按钮</el-button>
+      <el-button type="primary">主要按钮 测试提交2_免密登录</el-button>
       <el-button type="success">成功按钮</el-button>
       <el-button type="info">信息按钮</el-button>
       <el-button type="warning">警告按钮</el-button>
